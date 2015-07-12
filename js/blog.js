9a11e2cfa6d54d5dcd36ffd403ed6a13eca88bd7
sssiddhant <siddhant9910@gmail.com>
2015-07-13 00:35:33 +0530

    change

---
+++ js/blog.js
@@ -0,0 +1,14 @@
+﻿$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("LsZ5uUaakzgsrZA2Q9Si4DA3tQ5ZqkvfAtMpQiyE", "2TPFEGQpaELEAQaQrzkFUpe6burE4VEaAygCIuYj");
+ 
+    var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+    testObject.save({foo: "bar"}).then(function(object) {
+      alert("yay! it worked");
+    });
+ 
+});
