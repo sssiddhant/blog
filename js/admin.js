487c6d528e79fb905e6440f6910c32c7dc16830a
sssiddhant <siddhant9910@gmail.com>
2015-07-13 17:25:19 +0530

    change

--- js/admin.js
+++ js/admin.js
@@ -1,16 +1,10 @@
 ﻿$(function() {
  
+    
     Parse.$ = jQuery;
  
     Parse.initialize("LsZ5uUaakzgsrZA2Q9Si4DA3tQ5ZqkvfAtMpQiyE", "2TPFEGQpaELEAQaQrzkFUpe6burE4VEaAygCIuYj");
  
-    var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-  alert("yay! it worked");
-});
- 
-});
 $('.form-signin').on('submit', function(e) {
  
     // Prevent Default Submit Event
