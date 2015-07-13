173d68e28fdb9c53889df4c2304ec62f52bed755
sssiddhant <siddhant9910@gmail.com>
2015-07-13 17:14:05 +0530

    change

---
+++ js/admin.js
@@ -0,0 +1,13 @@
+﻿$(function() {
+ 
+    Parse.$ = jQuery;
+ 
+    Parse.initialize("LsZ5uUaakzgsrZA2Q9Si4DA3tQ5ZqkvfAtMpQiyE", "2TPFEGQpaELEAQaQrzkFUpe6burE4VEaAygCIuYj");
+ 
+    var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});
+ 
+});
